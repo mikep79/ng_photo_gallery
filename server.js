a8697599e3dea7ea4f292d4871831c3b927bc785
Mike Pettman <mp_palmatum@yahoo.com>
2017-09-28 12:27:21 -0500

    heroku port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var port = 5111;
+var port = process.env.PORT || 5111;    // use this port if on heroku, OR is local and use 5111.
 var index = require('./routes/index.js');
 var pics = require('./routes/pics.js');
 
